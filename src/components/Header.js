880ac5c96f73d8779ba97d6ca77ee6e21066addc
Rob Armitage <robert.c.armitage@gmail.com>
2020-04-09 23:45:36 +0100

    small CSS changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Header = () => (
   <div>
-    <h4>Sports Scoring App</h4>
-    <p>Inspired by CodeClan</p>
+    <h1>Ultimate Hackathon League</h1>
+    <p class="inspired-by-codeclan">Inspired by CodeClan</p>
   </div>
 )
 
