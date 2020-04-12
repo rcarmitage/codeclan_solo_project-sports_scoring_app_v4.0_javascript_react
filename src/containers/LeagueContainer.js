286a07b19b26665d0ef55c4d61269798c2037c38
Rob Armitage <robert.c.armitage@gmail.com>
2020-04-12 20:08:00 +0100

    add basic code to LeagueContainer.js, need to refactor from original components pages

---
+++ src/containers/LeagueContainer.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Header from '../components/Header';
+import NavBar from '../components/NavBar';
+import Home from '../components/Home';
+import TeamsComponent from '../components/teams/TeamsComponent';
+import TeamView from '../components/teams/TeamView';
+import AddTeam from '../components/teams/AddTeam';
+import EditTeam from '../components/teams/EditTeam';
+import FixturesComponent from '../components/fixtures/FixturesComponent';
+import FixtureView from '../components/fixtures/FixtureView';
+import AddFixture from '../components/fixtures/AddFixture';
+import EditFixture from '../components/fixtures/EditFixture';
+import LeagueTable from '../components/LeagueTable';
+import About from '../components/About';
+import ErrorPage from '../components/ErrorPage';
+
+class LeagueContainer extends Component {
+  render() {
+    return (
+      <Router>
+        <React.Fragment>
+          <Header />
+          <NavBar id="nav" />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/teams" component={TeamsComponent} />
+            <Route exact path="/fixtures" component={FixturesComponent} />
+            <Route exact path="/league-table" component={LeagueTable} />
+            <Route exact path="/about" component={About} />
+            <Route component={ErrorPage} />
+          </Switch>
+        </React.Fragment>
+      </Router>
+    );
+  }
+}
+
+export default LeagueContainer;
