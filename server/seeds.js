c09a4e9ad80aa3971f50fa175bd2caff2fe2b551
Rob Armitage <robert.c.armitage@gmail.com>
2020-04-19 23:54:10 +0100

    still working on seeds.js

--- server/seeds.js
+++ server/seeds.js
@@ -1,53 +1,52 @@
 const express = require('express');
 
-const teamsData = [
-  {
-
-  },
-  {
-    name: "Shop Winventory",
-    played: 2,
-    won: 2,
-    lost: 0,
-    points: 2
-  },
-  {
-    name: "Spending Tracker Big Spenders",
-    played: 2,
-    won: 0,
-    lost: 2,
-    points: 0
-  },
-  {
-    name: "Sports Scorers",
-    played: 2,
-    won: 2,
-    lost: 2,
-    points: 0
-  },
-  {
-    name: "Travel Bucket List Jetsetters",
-    played: 2,
-    won: 1,
-    lost: 1,
-    points: 1
-  },
-  {
-    name: "Vet Squad Managers",
-    played: 2,
-    won: 0,
-    lost: 2,
-    points: 0
-  },
-]
-
 const team01 = team.post({
-    name: "The Gym Bunnies",
-    played: 2,
-    won: 1,
-    lost: 1,
-    points: 1
-  });
+  name: "The Gym Bunnies",
+  played: 2,
+  won: 1,
+  lost: 1,
+  points: 1
+});
+
+const team02 = team.post({
+  name: "Shop Winventory",
+  played: 2,
+  won: 2,
+  lost: 0,
+  points: 2
+});
+
+const team03 = team.post({
+  name: "Spending Tracker Big Spenders",
+  played: 2,
+  won: 0,
+  lost: 2,
+  points: 0
+});
+
+const team04 = team.post({
+  name: "Sports Scorers",
+  played: 2,
+  won: 2,
+  lost: 2,
+  points: 0
+});
+
+const team05 = team.post({
+  name: "Travel Bucket List Jetsetters",
+  played: 2,
+  won: 1,
+  lost: 1,
+  points: 1
+});
+
+const team06 = team.post({
+  name: "Vet Squad Managers",
+  played: 2,
+  won: 0,
+  lost: 2,
+  points: 0
+});
 
 const fixturesData = [
   // TODO: work out how to use above teams for fixturesData
