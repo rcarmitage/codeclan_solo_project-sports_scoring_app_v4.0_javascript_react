3e964fb5846a359fb3b3a2d31bea7174573f7f4a
Rob Armitage <robert.c.armitage@gmail.com>
2020-04-20 23:43:27 +0100

    not sure what to do about this seeds.js file

--- server/seeds.js
+++ server/seeds.js
@@ -1,52 +1,52 @@
 const express = require('express');
 
-const team01 = team.post({
+const team01 = {
   name: "The Gym Bunnies",
   played: 2,
   won: 1,
   lost: 1,
   points: 1
-});
+};
 
-const team02 = team.post({
+const team02 = {
   name: "Shop Winventory",
   played: 2,
   won: 2,
   lost: 0,
   points: 2
-});
+};
 
-const team03 = team.post({
+const team03 = {
   name: "Spending Tracker Big Spenders",
   played: 2,
   won: 0,
   lost: 2,
   points: 0
-});
+};
 
-const team04 = team.post({
+const team04 = {
   name: "Sports Scorers",
   played: 2,
   won: 2,
   lost: 2,
   points: 0
-});
+};
 
-const team05 = team.post({
+const team05 = {
   name: "Travel Bucket List Jetsetters",
   played: 2,
   won: 1,
   lost: 1,
   points: 1
-});
+};
 
-const team06 = team.post({
+const team06 = {
   name: "Vet Squad Managers",
   played: 2,
   won: 0,
   lost: 2,
   points: 0
-});
+};
 
 const fixturesData = [
   // TODO: work out how to use above teams for fixturesData
