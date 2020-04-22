feb95fd5df6375a47223ce3dbfc15b890051dda5
Rob Armitage <robert.c.armitage@gmail.com>
2020-04-22 14:52:52 +0100

    updated server.js

--- server/server.js
+++ server/server.js
@@ -24,7 +24,7 @@ const Pool = require('pg').Pool
 const pool = new Pool({
   user: 'postgres',
   host: 'localhost',
-  password: 'Gl55sql',
+  password: 'password',
   database: 'sports_scoring_app_v4.0'
 });
 
