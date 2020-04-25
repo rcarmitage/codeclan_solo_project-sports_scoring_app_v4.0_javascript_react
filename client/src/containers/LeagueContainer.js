486ab971123a5e3ff7fbfb4f028ef98a8ad25c1e
Rob Armitage <robert.c.armitage@gmail.com>
2020-04-25 13:06:10 +0100

    componentDidMount: removed .this(res => res.teams) and changed 'data' to 'teams', did the same for fixtures, server.js example teams now show up as props in TeamsComponent but aren't being passed down to TeamList to be rendered

--- client/src/containers/LeagueContainer.js
+++ client/src/containers/LeagueContainer.js
@@ -4,13 +4,13 @@ import Header from '../components/Header';
 import NavBar from '../components/NavBar';
 import Home from '../components/Home';
 import TeamsComponent from '../components/teams/TeamsComponent';
-import TeamView from '../components/teams/TeamView';
-import AddTeam from '../components/teams/AddTeam';
-import EditTeam from '../components/teams/EditTeam';
+// import TeamView from '../components/teams/TeamView';
+// import AddTeam from '../components/teams/AddTeam';
+// import EditTeam from '../components/teams/EditTeam';
 import FixturesComponent from '../components/fixtures/FixturesComponent';
-import FixtureView from '../components/fixtures/FixtureView';
-import AddFixture from '../components/fixtures/AddFixture';
-import EditFixture from '../components/fixtures/EditFixture';
+// import FixtureView from '../components/fixtures/FixtureView';
+// import AddFixture from '../components/fixtures/AddFixture';
+// import EditFixture from '../components/fixtures/EditFixture';
 import LeagueTable from '../components/LeagueTable';
 import About from '../components/About';
 import ErrorPage from '../components/ErrorPage';
@@ -25,12 +25,20 @@ class LeagueContainer extends Component {
     }
   }
 
+  // componentDidMount() {
+  //   const url = 'http://localhost:3005/api/teams';
+
+  //   fetch(url)
+  //     .then(res => res.json())
+  //     .then(teams => this.setState({ teams: teams }))
+  //     .catch(err => console.error);
+  // }
+
   componentDidMount() {
     const promises =[
       fetch('http://localhost:3005/api/teams')
         .then(res => res.json())
-        .then(res => res.teams)
-        .then(data => this.setState({ teams: data }))
+        .then(teams => this.setState({ teams: teams }))
       ,
       fetch('http://localhost:3005/api/fixtures')
         .then(res => res.json())
