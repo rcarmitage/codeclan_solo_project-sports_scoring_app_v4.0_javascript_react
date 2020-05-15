164403d3b2450b1efbd2b0b10a114effdd7fd96c
Rob Armitage <robert.c.armitage@gmail.com>
2020-05-16 00:03:35 +0100

    started implementing Context

---
+++ client/src/context/types.js
@@ -0,0 +1,5 @@
+export const GET_TEAMS = "GET_TEAMS";
+export const GET_TEAM = "GET_TEAM";
+export const ADD_TEAM = "ADD_TEAM";
+export const EDIT_TEAM = "EDIT_TEAM";
+export const DELETE_TEAM = "DELETE_TEAM";
