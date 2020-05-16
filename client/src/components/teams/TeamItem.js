d67069fba97e304114ef186394708d6820b691ba
Rob Armitage <robert.c.armitage@gmail.com>
2020-05-16 23:32:47 +0100

    TeamItem: trying team passed as props + deleteTeam from Context

--- client/src/components/teams/TeamItem.js
+++ client/src/components/teams/TeamItem.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 // import PropTypes from 'prop-types';
 import LeagueContext from '../../context/league/leagueContext';
 
-const TeamItem = () => {
+const TeamItem = ({ team: name, team_id }) => {
   const leagueContext = useContext(LeagueContext);
 
   const { team, deleteTeam } = leagueContext;
