806b455a03f41ff421b5c14d543ba808ea16eb83
Rob Armitage <robert.c.armitage@gmail.com>
2020-05-21 19:00:11 +0100

    working through CRUD using team ids instead of names first; getFixtures renders fixtures with team_a_id, team_b_id and winning_team_id

--- client/src/components/fixtures/FixtureItem.js
+++ client/src/components/fixtures/FixtureItem.js
@@ -11,7 +11,7 @@ const FixtureItem = ({ fixture }) => {
     clearCurrentFixture,
   } = leagueContext;
 
-  const { fixture_id, team_a, team_b, winning_team } = fixture;
+  const { fixture_id, team_a_id, team_b_id, winning_team_id } = fixture;
 
   const onDelete = () => {
     deleteFixture(fixture_id);
@@ -22,14 +22,14 @@ const FixtureItem = ({ fixture }) => {
     <Fragment>
       <div className='fixtures-container'>
         <div className='fixtures-container-top-row'>
-          <p className='team-a'>{team_a}</p>
+          <p className='team-a'>{team_a_id}</p>
           <p className='vs'>vs</p>
-          <p className='team-b'>{team_b}</p>
+          <p className='team-b'>{team_b_id}</p>
         </div>
 
         <div className='fixtures-container-middle-row'>
           <p className='winner-text'>Winner:</p>
-          <p className='winner-team'>{winning_team}</p>
+          <p className='winner-team'>{winning_team_id}</p>
         </div>
 
         <div className='fixtures-container-bottom-row'>
